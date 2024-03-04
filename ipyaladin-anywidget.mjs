708b75dfa0db183d49c54a2aaaa08af6727b735d
MARCHAND MANON <manonmarchand22@gmail.com>
2024-03-04 18:14:36 +0100

    add target and coordinates

--- ipyaladin-anywidget.mjs
+++ ipyaladin-anywidget.mjs
@@ -1,20 +1,42 @@
 import A from "https://esm.sh/aladin-lite@3.2.0";
 
+let idxView = 0
 
 export function render({model, el}) {
 
     A.init.then(() => {
 
+        let height = model.get("height");
+        let target = model.get("target");
 
-        const height = () => model.get("height");
-        el.setAttribute("style",`width:100%;height:${height()}px;`);
-        el.id = 'aladin-lite-div';
-        console.log(el);
+        el.setAttribute("style",`width:100%;height:${height}px;`);
+        el.id = `aladin-lite-div-${idxView}`;
 
-        A.aladin('#aladin-lite-div', {projection: "TAN", target: '15 16 57.636 -60 55 7.49', showCooGrid: true, fov: 90});
-        console.log(el);
+        let aladin = new A.aladin(`#aladin-lite-div-${idxView}`, {projection: "TAN", target: target, showCooGrid: true, fov: 90});
+
         
+        aladin.on("positionChanged", (position) => {
+          model.set('ra', position.ra);
+          model.set('dec', position.dec);
+          console.log(model.get('ra'), ' ' ,model.get('dec'))
+          model.save_changes();
+        })
 
-    })
+        model.on("change:height", () => {
+            height = model.get("height");
+            el.style.height = `${height}px`;
+        });
 
+        model.on("change:target", () => {
+          target = model.get("target");
+          
+          aladin.gotoObject(target)
+
+          console.log(aladin.getRaDec())
+        })
+
+        el.appendChild(aladin);
+
+    });
+    
 }
